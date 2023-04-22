988aac6937aedf5c08b4ba3f86b94f1a3d920270
anandhakrishnan-ar <anandhank77@gmail.com>
2023-04-22 19:25:59 +0530

    sub string check

---
+++ SubStringCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SubStringCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello world";
+            Console.WriteLine(str.IndexOf("llo"));
+        }
+    }
+}
